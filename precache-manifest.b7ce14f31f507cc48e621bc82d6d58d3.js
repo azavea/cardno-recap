0b2e03ad824d300285031cce2cdc2532bcafccd4
Kathryn Killebrew <kkillebrew@azavea.com>
2019-08-26 14:05:40 -0400

    Updates

---
+++ precache-manifest.b7ce14f31f507cc48e621bc82d6d58d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31cb21311f8f799b2037427709aa6f3",
+    "url": "/cardno-recap/index.html"
+  },
+  {
+    "revision": "80d4db1e6730f1773267",
+    "url": "/cardno-recap/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "d24a2296214673512659",
+    "url": "/cardno-recap/static/js/2.539c6abd.chunk.js"
+  },
+  {
+    "revision": "80d4db1e6730f1773267",
+    "url": "/cardno-recap/static/js/main.880ef4c8.chunk.js"
+  },
+  {
+    "revision": "bf5b99b64effbf6b2de6",
+    "url": "/cardno-recap/static/js/runtime~main.88bf3d6a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cardno-recap/static/media/logo.5d5d9eef.svg"
+  }
+]);
