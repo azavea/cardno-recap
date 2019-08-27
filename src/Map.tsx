81f90591fda7264737b6f5048472680f0ac11123
Kathryn Killebrew <kkillebrew@azavea.com>
2019-08-27 15:16:43 -0400

    Add data layer
    Add styled tile layer for Djibouti data.
    Closes azavea/geospatial-apps-tasks#2.

--- src/Map.tsx
+++ src/Map.tsx
@@ -15,9 +15,10 @@ class RecapMap extends React.Component<Props, State> {
   constructor (props: Props) {
     super(props)
     this.state = {
-      lat: 39.9526,
-      lng: -75.1636,
-      zoom: 13
+      // Djibouti
+      lat: 11.6663,
+      lng: 42.6593,
+      zoom: 8
     }
   }
 
@@ -29,6 +30,10 @@ class RecapMap extends React.Component<Props, State> {
           attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           subdomains='abcd'
           url='https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.png'
+        />
+        <TileLayer
+          attribution='<a href="https://azavea.com">azavea</a>'
+          url='https://un-sdg.s3.amazonaws.com/tiles/worldpop/test/dji-burned-roads/magma/{z}/{x}/{y}.png'
         />
       </Map>
     );
